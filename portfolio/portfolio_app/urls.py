43083b23f5913706198de3a7470e4717e5c6ffa8
KHAJA_MUBASHIR_ARSALAN <kmarsalan124@gmail.com>
2025-12-10 18:32:48 +0530

    url added

--- portfolio/portfolio_app/urls.py
+++ portfolio/portfolio_app/urls.py
@@ -3,6 +3,9 @@ from . import views
 from .views import google_verification
 from django.contrib.sitemaps.views import sitemap
 from sitemaps import StaticViewSitemap
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -12,4 +15,8 @@ urlpatterns = [
     path("", views.portfolio, name="portfolio"),
     path('google906ecfcb3556e71a.html', google_verification, name='google_verification'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path(
+        "robots.txt",
+        RedirectView.as_view(url=staticfiles_storage.url("robots.txt"), permanent=True),
+    ),
 ]
